d9df6364a4e7cf7384ab15e4454c6d1742edca37
zhangruyi <2393850685@qq.com>
2019-11-04 00:09:52 +1400

    week8

---
+++ test2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a;
+static int b=10;
+int main(int argc,char* argv[],char* envp[])
+{
+	int a=10;
+	char* p=(char*)malloc(sizeof(char));
+	printf("a=%d,p=0x%x\n",a,p);//动态链接库，*.so
+	getchar();
+	getchar();
+	return 0;
+
+}
